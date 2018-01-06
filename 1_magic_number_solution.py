e289f2fe07499d74b318b6a088e71a723c675de6
Alice Gutteridge <alicegutteridge@gmail.com>
2018-01-06 20:15:31 +0000

    magic number solution

---
+++ 1_magic_number_solution.py
@@ -0,0 +1,17 @@
+# Create a variable called start_number, and make it a random number.  
+# Create a variable called doubled, and make it start_number multiplied by 2
+# Create a variable called plus_nine, and add 9 to doubled
+# Create a variable called minus_three, and subtract 3 from plus_nine
+# Create a variable called divide_two, and divide minus_three by 2
+# Create a variable called result, and subtract start_number from divide_two
+# Print the result
+
+start_number = 29872439847
+doubled = start_number * 2
+plus_nine = doubled + 9
+minus_three = plus_nine - 3
+divide_two = minus_three / 2
+result = divide_two - start_number
+
+print(result)
+print("The result is always 3!")
