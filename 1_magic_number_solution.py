da825ac4447fbdbf8cf162a69f6b5efcc9368d8d
Alice Gutteridge <agutteridge@hotels.com>
2018-01-17 17:08:41 +0000

    variable rename

--- 1_magic_number_solution.py
+++ 1_magic_number_solution.py
@@ -1,17 +1,17 @@
 # Create a variable called start_number, and make it a random number.  
-# Create a variable called doubled, and make it start_number multiplied by 2
-# Create a variable called plus_nine, and add 9 to doubled
-# Create a variable called minus_three, and subtract 3 from plus_nine
-# Create a variable called divide_two, and divide minus_three by 2
-# Create a variable called result, and subtract start_number from divide_two
+# Create a variable called step_1, and make it start_number multiplied by 2
+# Create a variable called step_2, and add 9 to step_1
+# Create a variable called step_3, and subtract 3 from step_2
+# Create a variable called step_4, and divide step_3 by 2
+# Create a variable called result, and subtract start_number from step_4
 # Print the result
 
 start_number = 29872439847
-doubled = start_number * 2
-plus_nine = doubled + 9
-minus_three = plus_nine - 3
-divide_two = minus_three / 2
-result = divide_two - start_number
+step_1 = start_number * 2
+step_2 = step_1 + 9
+step_3 = step_2 - 3
+step_4 = step_3 / 2
+result = step_4 - start_number
 
 print(result)
 print("The result is always 3!")
