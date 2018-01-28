2e0d5e8b3f76c1d66147a682b461cc6070ee4eef
Velislav Velinov <vevelinov@hotels.com>
2018-01-28 18:42:06 +0000

    Add solutions from lesson 2.

---
+++ 2_age_check_solution.py
@@ -0,0 +1,12 @@
+
+# Create a variable called age.
+# Write an if statement, which checks if someone of that age is allowed to drink. 
+# They are only if they're 18 and over.
+# If they are allowed - print "Can drink!".
+# If they are not - print "You have to wait!"
+
+age = 12
+if age >= 18:
+	print("Can drink!")
+else:
+	print("You have to wait!")
