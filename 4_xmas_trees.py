b74287b6d8d1833f459411786476048fd8eb46de
Alice Gutteridge <agutteridge@hotels.com>
2018-02-09 14:32:12 +0000

    🎄

--- 4_xmas_trees.py
+++ 4_xmas_trees.py
@@ -1,16 +1,19 @@
-# a) Write code that will print an n x m grid of the asterisk character for the screen.
-#    For example, if the user enters '3' and '4', the program should print out:
+# a) Write code that will print an n x m grid of the asterisk character for the
+#    screen. For example, if the user enters 3 and 4, the program should print:
 #
 #    ***
 #    ***
 #    ***
 #    ***
 
-# Challenge!
-# b) Write code that will print out an n row high triangle. For example if the user enters '4', 
-#    the program should print out:
+# b) Write code that will print out an n row high triangle. For example if the
+#    user enters '4', the program should print out:
 #
 #       *
 #      ***
 #     *****
 #    *******
+
+# HINT: you can multiply strings!
+stars = '*' * 3
+print(stars)
