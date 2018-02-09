fad12a8d4e08b5ff7e49f784994c951845dc04de
Alice Gutteridge <agutteridge@hotels.com>
2018-02-09 11:23:42 +0000

    added christmas trees exercise instructions

---
+++ 4_xmas_trees.py
@@ -0,0 +1,16 @@
+# a) Write code that will print an n x m grid of the asterisk character for the screen.
+#    For example, if the user enters '3' and '4', the program should print out:
+#
+#    ***
+#    ***
+#    ***
+#    ***
+
+# Challenge!
+# b) Write code that will print out an n row high triangle. For example if the user enters '4', 
+#    the program should print out:
+#
+#       *
+#      ***
+#     *****
+#    *******
