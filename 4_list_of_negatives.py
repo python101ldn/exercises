1482be08df0fff5e4f9749ef971019a5cb52a050
Alice Gutteridge <alicegutteridge@gmail.com>
2018-02-17 11:08:11 +0000

    forgot to save

--- 4_list_of_negatives.py
+++ 4_list_of_negatives.py
@@ -4,8 +4,8 @@ index = 0
 negatives = []
 
 while index < len(random_numbers):
-	if random_numbers[index] < 0:
-		negatives.append(random_numbers[index])
-	index = index + 1
+    if random_numbers[index] < 0:
+        negatives.append(random_numbers[index])
+    index = index + 1
 
 print(negatives)
