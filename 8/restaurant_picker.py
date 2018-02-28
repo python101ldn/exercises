a9558721c1015c4322191ac979fdc95b4f47eb9c
Alice Gutteridge <agutteridge@hotels.com>
2018-02-28 16:57:19 +0000

    restaurant code

---
+++ 8/restaurant_picker.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+
+# Do not edit - we need this import :)
+import csv
+
+# Do not edit - the dictionary should be empty to begin with!
+restaurants = {}
+
+
+# Do not edit
+def show_menu():
+    print('*** MENU ***')
+    print('1: Search based on distance')
+    print('2: Search based on star rating')
+    print('3: Add a new entry')
+    print('4: Exit\n')
+
+
+# TODO
+def search_on_distance(max_distance):
+    print('\n*** DISTANCE RESULTS ***')
+    # 1. Loop over the restaurants dict
+    # 2. Compare the distance of each restaurant to max_distance
+
+
+# TODO
+def search_on_rating(min_stars):
+    print('\n*** RATING RESULTS ***')
+    # 1. Loop over the restaurants dict
+    # 2. Compare the star_rating of each restaurant to min_stars
+
+
+# TODO
+def add_restaurant():
+    # 1. Create variables for each value you need from the user
+    # 2. Use those variables to create a dictionary, and also add it to the
+    # restaurants dictionary on line 5
+
+    restaurants['your variable for the name goes here'] = {
+        'cuisine': 'your variable for cuisine goes here!',
+        'cost': 'your variable for cost goes here!',
+        'star_rating': 'your variable for star_rating goes here!',
+        'distance': 'your variable for distance goes here!'
+    }
+
+
+# Do not edit
+def save_changes():
+    with open('restaurants.csv', 'w') as csvfile:
+        csv_writer = csv.DictWriter(
+            csvfile,
+            fieldnames=['name', 'cuisine', 'cost', 'star_rating', 'distance'])
+        csv_writer.writeheader()
+
+        for restaurant_name in restaurants.keys():
+            rest_details = restaurants[restaurant_name]
+            rest_details['name'] = restaurant_name
+            csv_writer.writerow(rest_details)
+
+
+# Do not edit
+def read_csvfile():
+    with open('restaurants.csv', 'r') as csvfile:
+        csv_reader = csv.DictReader(csvfile)
+
+        for rest_details in csv_reader:
+            restaurant_name = rest_details['name']
+            restaurants[restaurant_name] = rest_details
+
+# Execution starts from here
+read_csvfile()
+keep_going = True
+
+while keep_going:
+    show_menu()
+    choice = input('Please enter choice: ')
+    if choice == '1':
+        distance = input('Please enter max distance: ')
+        search_on_distance(distance)
+        print('\n')
+    elif choice == '2':
+        # TODO
+        print('Call the function search_on_rating!')
+        print('\n')
+    elif choice == '3':
+        # TODO
+        print('Call the function add_restaurant')
+        print('Call the function save_changes')
+        print('\n')
+    elif choice == '4':
+        # TODO
+        print('How do we prevent an infinite while loop?')
+        print('\n')
+    else:
+        print('That\'s not a valid choice - try again!')
+        print('\n')
---
+++ 8/restaurant_picker_solution.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+
+import csv
+
+restaurants = {}
+
+
+def show_menu():
+    print('*** MENU ***')
+    print('1: Search based on distance')
+    print('2: Search based on star rating')
+    print('3: Add a new entry')
+    print('4: Exit\n')
+
+
+def search_on_distance(max_distance):
+    print('\n*** DISTANCE RESULTS ***')
+
+    for restaurant_name in restaurants.keys():
+        restaurant = restaurants[restaurant_name]
+
+        if int(restaurant['distance']) <= int(max_distance):
+            print(
+                restaurant_name + ' is a ' + restaurant['cuisine'] +
+                ' place ' + restaurant['distance'] + ' minute(s) from here')
+
+
+def search_on_rating(min_stars):
+    print('\n*** RATING RESULTS ***')
+
+    for restaurant_name in restaurants.keys():
+        restaurant = restaurants[restaurant_name]
+
+        if int(restaurant['star_rating']) >= int(min_stars):
+            print(
+                restaurant_name + ' is a ' + restaurant['star_rating'] +
+                '/5 rated place ' + str(restaurant['distance']) +
+                ' minutes from here')
+
+
+def add_restaurant():
+    name = input('Enter Restaurant Name: ')
+    cuisine = input('Enter Restaurant cuisine: ')
+    cost = input('Enter Restaurant cost (out of 5): ')
+    star_rating = input('Enter star rating (out of 5): ')
+    distance = input('Enter distance (minutes\' walk): ')
+
+    restaurants[name] = {
+        'cuisine': cuisine,
+        'cost': cost,
+        'star_rating': star_rating,
+        'distance': distance
+    }
+
+
+def save_changes():
+    with open('restaurants.csv', 'w') as csvfile:
+        csv_writer = csv.DictWriter(
+            csvfile,
+            fieldnames=['name', 'cuisine', 'cost', 'star_rating', 'distance'])
+        csv_writer.writeheader()
+
+        for restaurant_name in restaurants.keys():
+            rest_details = restaurants[restaurant_name]
+            rest_details['name'] = restaurant_name
+            csv_writer.writerow(rest_details)
+
+
+def read_csvfile():
+    with open('restaurants.csv', 'r') as csvfile:
+        csv_reader = csv.DictReader(csvfile)
+
+        for rest_details in csv_reader:
+            restaurant_name = rest_details['name']
+            restaurants[restaurant_name] = rest_details
+
+
+# Execution starts from here
+read_csvfile()
+keep_going = True
+
+while keep_going:
+    show_menu()
+    choice = input('Please enter choice: ')
+    if choice == '1':
+        distance = input('Please enter max distance: ')
+        search_on_distance(distance)
+        print('\n')
+    elif choice == '2':
+        search_on_rating(input('Please enter minimum stars: '))
+        print('\n')
+    elif choice == '3':
+        add_restaurant()
+        save_changes()
+        print('\n')
+    elif choice == '4':
+        keep_going = False
+    else:
+        print('That\'s not a valid choice - try again!')
