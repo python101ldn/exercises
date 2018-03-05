457ded23c84f751c17708e10f4e8cc064fa502ad
Alice Gutteridge <agutteridge@hotels.com>
2018-03-05 11:26:04 +0000

    supermarket exercise

---
+++ 7/supermarket.py
@@ -0,0 +1,29 @@
+
+shopping_cart = {
+	'banana': 3,
+	'orange': 2
+}
+
+prices = {
+    'banana': 4,
+    'apple': 2,
+    'orange': 1.5,
+    'pear': 3
+}
+
+
+# 1. Define a function that takes two arguments: food and number of items,
+# and returns the cost of those items using the prices dictionary
+# e.g. you're buying 2 oranges, this should cost £3 in total
+
+# 2. Define a function that returns the cost for your whole cart
+
+# 3. Define a function that, using the below stock_levels dictionary, only adds to the total
+# if the item is in stock
+
+stock_levels = {
+    'banana': 6,
+    'apple': 0,
+    'orange': 32,
+    'pear': 15
+}
