63ffa4ba173ffa950916e2273188705fb5763e23
Alice Gutteridge <agutteridge@hotels.com>
2018-05-01 15:00:21 +0100

    remove fancy solution

--- Session_5/reverse_solution.py
+++ Session_5/reverse_solution.py
@@ -9,12 +9,3 @@ for letter in user_text:
     reversed = letter + reversed
 
 print(reversed)
-
-# another solution, more advanced :)
-# If this is confusing try printing:
-# for i in range(5, -1, -1):
-#     print(i)
-for position in range(len(user_text) - 1, -1, -1):
-    # The end="" bit prevents the print() from ending the line each time
-    print(user_text[position], end="")
-print()
