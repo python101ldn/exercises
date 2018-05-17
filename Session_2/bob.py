f25e74c64db51fd91f3a1e6613a11b5d4177312c
Alice Gutteridge <agutteridge@hotels.com>
2018-05-17 11:24:30 +0100

    clarify bob instructions

--- Session_2/bob.py
+++ Session_2/bob.py
@@ -1,8 +1,8 @@
 # Bob is a lackadaisical teenager.
 # In conversation, his responses are very limited.
 #
-# Bob answers 'Sure.' if you ask him a question.
-# He answers 'Whoa, chill out!' if you yell at him.
-# He answers 'Calm down, I know what I'm doing!' if you yell a question at him.
-# He says 'Fine. Be that way!' if you don't say anything.
+# Bob answers 'Sure.' if you ask him a question (with ?).
+# He answers 'Whoa, chill out!' if you yell at him (with !).
+# He answers 'Calm down, I know what I'm doing!' if you yell a question at him (with ! and ?).
+# He says 'Fine. Be that way!' if you don't say anything (an empty string '').
 # He answers 'Whatever.' to anything else.
