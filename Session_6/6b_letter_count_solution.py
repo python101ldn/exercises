034e154980cd9bd9594cc84e46744a4c78a3c226
Alice Gutteridge <agutteridge@hotels.com>
2018-05-31 13:50:47 +0100

    alternative solution

--- Session_6/6b_letter_count_solution.py
+++ Session_6/6b_letter_count_solution.py
@@ -8,3 +8,10 @@ for letter in word:
         letter_count[letter] = 1
 
 print(letter_count)
+
+# alternative solution: using a default value!
+for letter in word:
+    letter_count[letter] = letter_count.get(letter, 0) + 1
+
+print(letter_count)
+
