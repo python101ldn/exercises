dbdb8c2c79c22b72872db3ba87833dee2123c910
Alice Gutteridge <agutteridge@hotels.com>
2019-05-09 14:07:50 +0100

    simplified Bob, moved alarm to extra content, moved count up while exercise to week 2

---
+++ Session_2/2d_bob.py
@@ -0,0 +1,7 @@
+# Bob is a lackadaisical teenager.
+# In conversation, his responses are very limited.
+#
+# Bob answers 'Hey.' if you say 'hello' to him.
+# He answers 'Woah, hey!' if you say 'HELLO' to him.
+# He answers 'Laters.' if you say 'bye', 'BYE' or 'goodbye' to him.
+# He answers 'Whatever.' to anything else.
